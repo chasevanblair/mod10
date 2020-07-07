cd7771a52ed8437f679c679b110678312eb356d6
chasevanblair <crvanblair@dmacc.edu>
2020-07-07 18:32:27 -0500

    3

--- class_definitions/student.py
+++ class_definitions/student.py
@@ -1,7 +1,27 @@
+"""
+
+Program: student.py
+
+Author: Chase Van Blair
+
+Last date modified: 7/7/20
+
+
+The purpose of this program is to create a basic class
+to be used for testing
+
+"""
 class Student:
     """Student class"""
 
     def __init__(self, lname, fname, major, gpa=0.0):
+        """
+        Student constructor
+        :param lname: student's last name
+        :param fname: student's first name
+        :param major: student's major
+        :param gpa: student's gpa
+        """
         if not lname or not fname or not major:
             print("input error")
         self.last_name = lname
@@ -10,5 +30,9 @@ class Student:
         self.gpa = gpa
 
     def __str__(self):
+        """
+        outputs a readable version of Student
+        :return:
+        """
         return self.last_name + ", " + self.first_name + " has major " + self.major + " with gpa: " + str(self.gpa)
 
